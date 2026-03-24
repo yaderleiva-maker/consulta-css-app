6cea1f2718e1b6f57d0be672015f48eabf552a91
yaderleiva-maker <yaderleiva@gmail.com>
2026-03-24 17:08:44 -0500

    Update login.py

--- modulos/login.py
+++ modulos/login.py
@@ -1,1 +1,39 @@
+import streamlit as st
 
+# Usuarios (por ahora simple)
+usuarios = {
+    "yader@gmail.com": "1234",
+    "supervisor@gmail.com": "abcd"
+}
+
+def login():
+
+    if "login_ok" not in st.session_state:
+        st.session_state.login_ok = False
+
+    if "usuario" not in st.session_state:
+        st.session_state.usuario = None
+
+    if not st.session_state.login_ok:
+
+        st.title("🔐 Acceso a la plataforma")
+
+        usuario = st.text_input("Correo")
+        password = st.text_input("Contraseña", type="password")
+
+        if st.button("Ingresar"):
+            if usuario in usuarios and usuarios[usuario] == password:
+                st.session_state.login_ok = True
+                st.session_state.usuario = usuario
+                st.success("Acceso concedido ✅")
+                st.rerun()
+            else:
+                st.error("Credenciales incorrectas ❌")
+
+        st.stop()
+
+def logout():
+    if st.button("Cerrar sesión"):
+        st.session_state.login_ok = False
+        st.session_state.usuario = None
+        st.rerun()

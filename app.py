4e8b64c8108ec73e3d9d75d2b4fb87d1294328b9
yaderleiva-maker <yaderleiva@gmail.com>
2026-03-25 15:02:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ login.login()
 # LOGOUT
 login.logout()
 
-# SOLO ejecutar consultas si está logueado
+# SOLO si está logueado mostramos el sistema
 if st.session_state.get("login_ok"):
-    consultas.run(st.session_state.get("usuario"))
-# MENÚ
-opcion = st.sidebar.selectbox(
-    "Selecciona módulo",
-    ["Consultas"]
-)
 
-# NAVEGACIÓN
-if opcion == "Consultas":
-    consultas.run(st.session_state.usuario)
+    # MENÚ
+    opcion = st.sidebar.selectbox(
+        "Selecciona módulo",
+        ["Consultas"]
+    )
+
+    # NAVEGACIÓN
+    if opcion == "Consultas":
+        consultas.run(st.session_state.get("usuario"))

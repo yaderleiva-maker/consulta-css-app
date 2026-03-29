a18e80fefb1e0ce40de3d5cead58537ccbd6d402
yaderleiva-maker <yaderleiva@gmail.com>
2026-03-29 18:35:52 -0500

    Update consultas.py

--- modulos/consultas.py
+++ modulos/consultas.py
@@ -88,6 +88,8 @@ def run(usuario):
         # -----------------------
         # QUERY
         # -----------------------
+    if tipo_consulta == "CSS":
+
         query = """
         SELECT 
           a.cedula,
@@ -101,6 +103,51 @@ def run(usuario):
         LEFT JOIN `proyecto-css-panama.css_data.css-actual` b
         ON a.cedula = b.cedula
         """
+
+    elif tipo_consulta == "TELÉFONOS NUEVOS":
+
+        query = """
+        WITH archivo AS (
+          SELECT cedula, telf1 AS numero FROM `proyecto-css-panama.consultas.temp_clientes`
+          UNION ALL SELECT cedula, telf2 FROM `proyecto-css-panama.consultas.temp_clientes`
+          UNION ALL SELECT cedula, telf3 FROM `proyecto-css-panama.consultas.temp_clientes`
+          UNION ALL SELECT cedula, telf4 FROM `proyecto-css-panama.consultas.temp_clientes`
+          UNION ALL SELECT cedula, telf5 FROM `proyecto-css-panama.consultas.temp_clientes`
+          UNION ALL SELECT cedula, telf6 FROM `proyecto-css-panama.consultas.temp_clientes`
+          UNION ALL SELECT cedula, telf7 FROM `proyecto-css-panama.consultas.temp_clientes`
+          UNION ALL SELECT cedula, telf8 FROM `proyecto-css-panama.consultas.temp_clientes`
+          UNION ALL SELECT cedula, telf9 FROM `proyecto-css-panama.consultas.temp_clientes`
+          UNION ALL SELECT cedula, telf10 FROM `proyecto-css-panama.consultas.temp_clientes`
+        ),
+
+        archivo_limpio AS (
+          SELECT 
+            cedula,
+            numero,
+            CONCAT(cedula, numero) AS clave
+          FROM archivo
+          WHERE numero IS NOT NULL AND numero != ''
+        ),
+
+        bigquery_data AS (
+          SELECT 
+            cedula,
+            numero,
+            CONCAT(cedula, numero) AS clave
+          FROM `proyecto-css-panama.css_data.telefonos-actual`
+        )
+
+        SELECT 
+          b.cedula,
+          b.numero
+        FROM bigquery_data b
+        LEFT JOIN archivo_limpio a
+        ON b.clave = a.clave
+        WHERE a.clave IS NULL
+        """
+
+# luego aquí ejecutas SIEMPRE igual
+result = client.query(query).to_dataframe()
 
         result = client.query(query).to_dataframe()
 

0934bf70f50a96d7079ffc2a0320a02f8d41a147
yaderleiva-maker <yaderleiva@gmail.com>
2026-04-22 13:50:04 -0500

    Update hopsa.py

--- modulos/hopsa.py
+++ modulos/hopsa.py
@@ -1,6 +1,12 @@
 import streamlit as st
+import pandas as pd
+import datetime
+from google.cloud import bigquery
+from google.oauth2 import service_account
 
 def run(usuario):
-    st.title("HOPSA - PRUEBA")
+    st.title("HOPSA - PRUEBA 2")
     st.write(f"Usuario: {usuario}")
-    st.success("Si ves esto, el módulo carga correctamente")
+    st.write("Pandas version:", pd.__version__)
+    st.write("BigQuery importado correctamente")
+    st.success("Todas las importaciones OK")

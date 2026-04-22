d94a004bf550e5af53a78a8e483e7d818fe5acfb
yaderleiva-maker <yaderleiva@gmail.com>
2026-04-22 12:04:48 -0500

    Update hopsa.py

--- modulos/hopsa.py
+++ modulos/hopsa.py
@@ -11,7 +11,7 @@ import io
 # -------------------------------
 PROJECT_ID = "proyecto-css-panama"
 DATASET_HOPSA = "hopsa"  # Usando tu dataset existente
-TABLE_ASESORES = f"{PROJECT_ID}.{DATASET_HOPSA}.hopsa_asesores"
+TABLE_ASESORES = f"{PROJECT_ID}.{DATASET_HOPSA}.asesores"
 TABLE_REPORTE = f"{PROJECT_ID}.{DATASET_HOPSA}.hopsa_reporte_diario"
 TABLE_MANUAL = f"{PROJECT_ID}.{DATASET_HOPSA}.hopsa_datos_manuales"
 

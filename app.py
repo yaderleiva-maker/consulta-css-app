00cd80209dd4c693d34daae4a62b62ee58f073de
yaderleiva-maker <yaderleiva@gmail.com>
2026-06-11 23:17:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from modulos import login, carga_documentos
 from modulos.hexagon_panama.consultas import consultas
 from modulos.hexagon_panama.hopsa import hopsa
 from modulos.hexagon_panama.hopsa import control_almuerzos
+from modulos.crm import cargar_documentos
 from modulos import inventario
 
 

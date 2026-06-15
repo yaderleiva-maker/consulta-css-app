976f23985159517815921c2cb0572c6fa73ea73d
yaderleiva-maker <yaderleiva@gmail.com>
2026-06-15 16:07:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,11 @@ MODULOS = {
 # Permisos por usuario
 ROLES = {
     "yaderleiva@gmail.com": {
-
+        "CONSULTAS": True,
+        "CARGA_DOCUMENTOS": True,
+        "HOPSA": True,
+        "CONTROL_ALMUERZOS": True,
+        "INVENTARIO": True,
         "CARGA_REPORTES": True
     },
     "mariachacon@hopsa.com": {

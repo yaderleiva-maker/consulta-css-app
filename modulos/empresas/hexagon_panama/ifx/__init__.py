925b9068ad87795074c28c8d2c14a468cfa434e6
yaderleiva-maker <yaderleiva@gmail.com>
2026-08-05 14:30:03 -0500

    Add __init__.py for IFX module with exports

---
+++ modulos/empresas/hexagon_panama/ifx/__init__.py
@@ -0,0 +1,23 @@
+"""
+Módulo IFX - Dashboard Comercial y Análisis de Gestión
+"""
+
+from .dashboard import DashboardIFX
+from .embudo import EmbudoIFX
+from .reportes import ReportesIFX
+from .models import (
+    GestionIFX,
+    ClienteIFX,
+    ResumenKPI,
+    EmbudoEtapa
+)
+
+__all__ = [
+    'DashboardIFX',
+    'EmbudoIFX',
+    'ReportesIFX',
+    'GestionIFX',
+    'ClienteIFX',
+    'ResumenKPI',
+    'EmbudoEtapa'
+]
